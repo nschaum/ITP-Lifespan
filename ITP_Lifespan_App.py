88f98e1f6fcc739c0837323ecbbb968d25925141
nschaum <36319806+nschaum@users.noreply.github.com>
2023-04-01 11:51:26 -0700

    A better simple working model of the app! Adds full drug names

--- ITP_Lifespan_App.py
+++ ITP_Lifespan_App.py
@@ -11,19 +11,27 @@ from lifelines.statistics import logrank_test
 data_path = 'C:\\Users\\ndsch\\Data\\ITP-Lifespan-Data\\ITP_processed_data\\ITP_2004-2016_concat_simple.csv'
 df = pd.read_csv(data_path)
 
-# Streamlit app
-st.title("Kaplan Meier Survival Analysis")
+# Add a new column that combines treatment and full_name values
+df["treatment_fullname"] = df["treatment"] + ": " + df["full_name"]
 
-# Get unique treatment values
-treatment_values = sorted(df["treatment"].unique())
-selected_treatment = st.selectbox("Select a treatment", treatment_values)
+# Streamlit app
+st.sidebar.title("ITP Lifespan Browser")
+st.sidebar.write("This app allows you to view the survival curve for any* treatment tested by the Interventions Testing Program between 2004-2016.")
+st.sidebar.write("*Currently excludes special cases like combined treatments or treatments that were not continuous throughout the remaining lifespan. Future version will include these cases.")
+
+# Get unique treatment_fullname values
+treatment_fullname_values = sorted(df["treatment_fullname"].unique())
+selected_treatment_fullname = st.sidebar.selectbox("Select a treatment", treatment_fullname_values)
+
+# Get the corresponding treatment value from the selected treatment_fullname
+selected_treatment = selected_treatment_fullname.split(": ")[0]
 
 # Get unique combinations of Rx(ppm), age_initiation(mo), and cohort for the selected treatment
 unique_combinations = df.loc[df["treatment"] == selected_treatment, ["Rx(ppm)", "age_initiation(mo)", "cohort"]].drop_duplicates()
 unique_combinations["combo"] = unique_combinations["Rx(ppm)"].astype(str) + " ppm, " + unique_combinations["age_initiation(mo)"].astype(str) + " mo, " + unique_combinations["cohort"]
 
 # Let the user select a unique combination
-selected_combo = st.selectbox("Select a cohort (dose, age of initiation, and year)", unique_combinations["combo"].tolist())
+selected_combo = st.sidebar.selectbox("Select a cohort (dose, age of initiation, and year)", unique_combinations["combo"].tolist())
 selected_rx_ppm, selected_age_initiation, selected_cohort = selected_combo.split(", ")
 
 # Extract the numeric values from the selected_combo
@@ -32,10 +40,10 @@ selected_age_initiation = int(selected_age_initiation.split(" ")[0])
 
 # Let the user select the sex and site
 sex_values = ["m", "f", "m+f"]
-selected_sex = st.selectbox("Select sex (m, f, or m+f)", sex_values, index=2)
+selected_sex = st.sidebar.selectbox("Select sex (m, f, or m+f)", sex_values, index=2)
 
 site_values = ["TJL", "UM", "UT", "All"]
-selected_site = st.selectbox("Select site (TJL, UM, UT, or All)", site_values, index=3)
+selected_site = st.sidebar.selectbox("Select site (TJL, UM, UT, or All)", site_values, index=3)
 
 # Filter the data based on the user's selections
 selected_data = df[(df["treatment"] == selected_treatment) & (df["Rx(ppm)"] == selected_rx_ppm) & (df["age_initiation(mo)"] == selected_age_initiation) & (df["cohort"] == selected_cohort)]
@@ -70,6 +78,8 @@ fig, ax = plt.subplots(figsize=(10, 6))
 kmf.plot(ax=ax)
 kmf_control.plot(ax=ax)
 
+ax.set_title(selected_treatment_fullname, fontsize=20)
+
 ax.axhline(y=0.5, linestyle="--", color="gray")
 ax.axvline(x=selected_age_initiation * 30, linestyle="--", color="gray")
 ax.yaxis.set_major_locator(MultipleLocator(0.1))
@@ -88,10 +98,15 @@ ax.axvline(x=median_lifespan_control, ymin=0, ymax=0.5, linestyle="--", color=co
 
 # Add labels
 init_label = f"{selected_treatment} start at {selected_age_initiation}mos of age"
-ax.text(selected_age_initiation * 30 - 10, 0.01, init_label, rotation=90, va="bottom", ha="right", fontsize=10, color="gray")
+ax.text(selected_age_initiation * 30 - 10, 0.01, init_label, rotation=90, va="bottom", ha="right", fontsize=12, color="gray")
 
-ax.text(median_lifespan_selected + 10, 0.01, f"{median_lifespan_selected:.1f} days", rotation=90, va="bottom", ha="left", fontsize=10, color=selected_color)
-ax.text(median_lifespan_control - 10, 0.01, f"{median_lifespan_control:.1f} days", rotation=90, va="bottom", ha="right", fontsize=10, color=control_color)
+# Adds labels on a particular side of the line depending on where the lines are located so the labels don't overlap
+if median_lifespan_selected > median_lifespan_control:
+    ax.text(median_lifespan_selected + 10, 0.01, f"{median_lifespan_selected:.1f} days", rotation=90, va="bottom", ha="left", fontsize=12, color=selected_color)
+    ax.text(median_lifespan_control - 10, 0.01, f"{median_lifespan_control:.1f} days", rotation=90, va="bottom", ha="right", fontsize=12, color=control_color)
+else:
+    ax.text(median_lifespan_selected - 10, 0.01, f"{median_lifespan_selected:.1f} days", rotation=90, va="bottom", ha="right", fontsize=12, color=selected_color)
+    ax.text(median_lifespan_control + 10, 0.01, f"{median_lifespan_control:.1f} days", rotation=90, va="bottom", ha="left", fontsize=12, color=control_color)
 
 ax.legend(loc="upper right", fontsize=14)
 st.pyplot(fig)
@@ -100,11 +115,15 @@ st.pyplot(fig)
 median_lifespan = pd.DataFrame({"Treatment": [selected_treatment, "control"],
                                  "Median Lifespan (days)": [kmf.median_survival_time_, kmf_control.median_survival_time_],
                                  "Max Lifespan (days)": [selected_data["age(days)"].max(), control_data["age(days)"].max()]})
-st.write(median_lifespan.set_index("Treatment"), index=False)
+st.write(median_lifespan.set_index("Treatment"))
+
+# Calculate percentage difference between treatment and control median lifespans
+median_lifespan_diff = (kmf.median_survival_time_ - kmf_control.median_survival_time_) / kmf_control.median_survival_time_ * 100
+st.markdown(f"**Difference in median lifespan:** {median_lifespan_diff:.0f}%")
 
 # Log-rank test
 results = logrank_test(selected_data["age(days)"], control_data["age(days)"], event_observed_A=selected_data["dead"], event_observed_B=control_data["dead"])
-st.write("Log-Rank Test:")
+st.markdown("<u>Log-Rank Test</u>", unsafe_allow_html=True)
 st.markdown(f"  Test statistic: {results.test_statistic:.2f}")
 if results.p_value < 0.00001:
     st.markdown(f"  P-value: {results.p_value:.1e}")
